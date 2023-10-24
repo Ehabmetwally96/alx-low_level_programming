71bb92919d162a1ab76a7d85396c5c67054fd0fa
Ehabmetwally96 <ehabmetwally515@gmail.com>
2023-10-24 14:30:02 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,8 @@ int main(void)
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-		putchar(i);
-	for (i = 'A' ; i <= 'Z' ; i++)
-		putchar(i);
+		if (i != 'q' && i != 'e')
+			putchar(i);
 	putchar('\n');
 	return (0);
 }
